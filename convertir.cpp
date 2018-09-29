fa37f84f81c0a48b101183cff32c46d9808876f2
Enrique Navarro <enrique.navarro64@gmail.com>
2018-09-29 16:21:35 -0500

    agregar funcion menu

--- convertir.cpp
+++ convertir.cpp
@@ -1,20 +1,63 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+void menu();
+int tamanoArreglo(int numero);
+int numeroAArreglo(int numero, int tamano);
+void imprimirArreglo(int arreglo[],int tamano);
 
+int main(int argc, char const *argv[]) {
+  menu();
   return 0;
 }
-int leerArreglo(){
-  return arreglo;
-}
-int numeroAArreglo(){
-  return arreglo;
+void menu(){
+//  int veces,origen,destino;
+    int numero;
+/*  cout<<"Operaciones que deseas realizar: "
+    cin>>veces;
+    while(veces>0){
+    cout<<"Dame base origen: ";
+    cin>>origen;
+    cout<<"Dame base destino: ";
+    cin>>destino;*/
+    cout<<"Dame numero: ";
+    cin>>numero;
+    int tamano = tamanoArreglo(numero);
+    int arreglo[tamano];
+    *arreglo=numeroAArreglo(numero,tamano);
+    imprimirArreglo(arreglo,tamano);
+//  }
 }
 
-int arregloANumero(){
+int tamanoArreglo(int numero){
+  int tamano=0;
+  while(numero>0){
+    numero/=10;
+    tamano++;
+  }
+  return tamano;
+}
+
+/*int leerArreglo(){
+  return arreglo;
+}*/
+
+int numeroAArreglo(int numero, int tamano){
+  int arreglo[tamano];
+  for (int i = tamano-1; i >= 0; i--) {
+    arreglo[i] = numero % 10;
+    numero /= 10;
+  }
+  return arreglo[tamano];
+}
+
+/*int arregloANumero(int arreglo){
   return numero;
-}
+}*/
 
-void imprimirArreglo(){
+void imprimirArreglo(int arreglo[],int tamano){
+  for (int i = 0; i < tamano; i++) {
+    cout<<arreglo[i]<<"";
+  }
+  cout<<endl;
 }
