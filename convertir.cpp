edea46171092aacacb962bf17f7de6253c2507e8
Enrique Navarro <enrique.navarro64@gmail.com>
2018-09-29 15:50:07 -0500

    convertir.cpp

---
+++ convertir.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
+int leerArreglo(){
+  return arreglo;
+}
+int numeroAArreglo(){
+  return arreglo;
+}
+
+int arregloANumero(){
+  return numero;
+}
+
+void imprimirArreglo(){
+}
