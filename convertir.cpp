cb8be4d479f526d040987132632d59e1cab52127
Enrique Navarro <enrique.navarro64@gmail.com>
2018-09-29 19:35:35 -0500

    add menu

--- convertir.cpp
+++ convertir.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void menu();
 int tamanoArreglo(int numero);
-int numeroAArreglo(int numero, int tamano);
+void menu();
+int * numeroAArreglo(int n, int t);
 void imprimirArreglo(int arreglo[],int tamano);
 
 int main(int argc, char const *argv[]) {
@@ -12,7 +12,7 @@ int main(int argc, char const *argv[]) {
 }
 void menu(){
 //  int veces,origen,destino;
-    int numero;
+    int numero,tamano;
 /*  cout<<"Operaciones que deseas realizar: "
     cin>>veces;
     while(veces>0){
@@ -22,13 +22,16 @@ void menu(){
     cin>>destino;*/
     cout<<"Dame numero: ";
     cin>>numero;
-    int tamano = tamanoArreglo(numero);
-    int arreglo[tamano];
-    *arreglo=numeroAArreglo(numero,tamano);
+    int *arreglo;
+    tamano=tamanoArreglo(numero);
+    arreglo=numeroAArreglo(numero,tamano);
     imprimirArreglo(arreglo,tamano);
 //  }
 }
 
+/*int leerArreglo(){
+  return arreglo;
+}*/
 int tamanoArreglo(int numero){
   int tamano=0;
   while(numero>0){
@@ -37,18 +40,16 @@ int tamanoArreglo(int numero){
   }
   return tamano;
 }
-
-/*int leerArreglo(){
-  return arreglo;
-}*/
-
-int numeroAArreglo(int numero, int tamano){
-  int arreglo[tamano];
+int * numeroAArreglo(int n, int t){
+  int numero = n;
+  int tamano = t;
+  int *arreglo=new int[tamano];
   for (int i = tamano-1; i >= 0; i--) {
     arreglo[i] = numero % 10;
     numero /= 10;
   }
-  return arreglo[tamano];
+  return arreglo;
+//  imprimirArreglo(arreglo,tamano);
 }
 
 /*int arregloANumero(int arreglo){
@@ -57,7 +58,7 @@ int numeroAArreglo(int numero, int tamano){
 
 void imprimirArreglo(int arreglo[],int tamano){
   for (int i = 0; i < tamano; i++) {
-    cout<<arreglo[i]<<"";
+    cout<<arreglo[i];
   }
   cout<<endl;
 }
